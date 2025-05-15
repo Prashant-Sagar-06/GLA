ff0f21df604e7f78d66ed05f8ac357db003ca1a4
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2025-05-15 15:40:45 +0530

    Implement code changes to enhance functionality and improve performance

--- Cultural/All_Cultural_club.js
+++ Cultural/All_Cultural_club.js
@@ -23,3 +23,23 @@ function typeEffect() {
 }
 
 typeEffect();
+
+// ----------------- Smooth Scrolling -----------------
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1; // Reset auto cycle
+    showSlides();
+}
